c60416f97438a1fbc8599783525a292468e5f606
Vipul Vinod Patil <vipulvpatil@gmail.com>
2023-05-15 17:47:16 +0530

    Did a basic setup that displays the hero page correctly

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      oswald: ["var(--font-oswald)"],
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        bold: "#9368B2",
+        subtle: "#E7C6FF",
       },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      width: {
+        hero: "1040px",
+      },
+      fontSize: {
+        primary: "24px",
+      },
+      margin: {
+        relax: "-67.5px",
+        relaxSmall : "-39px",
+      }
     },
   },
   plugins: [],
