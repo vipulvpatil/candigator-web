61fefb1ec21ce4c167b3a45787d1923e04d3e436
Vipul Vinod Patil <vipulvpatil@gmail.com>
2023-05-22 16:18:59 +0530

    Added DB for NextAuth

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,8 +1,12 @@
 import GoogleProvider from "next-auth/providers/google"
 import NextAuth from "next-auth"
+import {PrismaAdapter} from "@next-auth/prisma-adapter"
+import {PrismaClient} from "@prisma/client"
+
+const prisma = new PrismaClient()
 
 export const authOptions = {
-  // Configure one or more authentication providers
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
