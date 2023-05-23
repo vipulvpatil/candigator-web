251d157d3a74dac5ddb44473827a24ae00551228
Vipul Vinod Patil <vipulvpatil@gmail.com>
2023-05-23 15:43:57 +0530

    revamped GRPC client

---
+++ src/lib/grpc/client.js
@@ -0,0 +1,58 @@
+import {credentials, loadPackageDefinition} from "@grpc/grpc-js"
+import {loadSync} from "@grpc/proto-loader"
+import path from "path"
+import {promisify} from "util"
+
+const PROTO_PATH = path.resolve("./src/protos/server.proto")
+const packageDefinition = loadSync(
+  PROTO_PATH,
+  {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true
+  }
+)
+
+const apiServer = loadPackageDefinition(packageDefinition).protos
+
+const decodeFromBase64 = (str) => {
+  const decoded = Buffer.from(str, "base64").toString("ascii")
+  return Buffer.from(decoded)
+}
+
+const newGRPCClient = () => {
+  const caCert = decodeFromBase64(process.env.CA_CERT_BASE64)
+  const clientKey = decodeFromBase64(process.env.CLIENT_KEY_BASE64)
+  const clientCert = decodeFromBase64(process.env.CLIENT_CERT_BASE64)
+
+  const channelArgs = {}
+
+  if (process.env.GRPC_SERVER_SSL_TARGET_NAME_OVERRIDE){
+    channelArgs["grpc.ssl_target_name_override"] = process.env.GRPC_SERVER_SSL_TARGET_NAME_OVERRIDE
+  }
+
+  const client = new apiServer.CandidateTrackerGo(
+    process.env.GRPC_SERVER,
+    credentials.createSsl(caCert, clientKey, clientCert),
+    channelArgs
+  )
+  return client
+}
+
+const grpcServiceClient = () => {
+  if (!global.grpcServiceClient || global.refreshGrpcClient) {
+    global.refreshGrpcClient = false
+    console.log("Creating new grpc client")
+    const client = newGRPCClient()
+    global.grpcServiceClient = {
+      client: client,
+      checkConnection: promisify(client.CheckConnection).bind(client)
+    }
+  }
+
+  return global.grpcServiceClient
+}
+
+export {grpcServiceClient}
--- src/lib/grpc/service.js
+++ src/lib/grpc/service.js
@@ -1,61 +1,13 @@
-import {Metadata, credentials, loadPackageDefinition} from "@grpc/grpc-js"
 import ErrorChecker from "@/lib/common/error_checker"
-import {loadSync} from "@grpc/proto-loader"
-import path from "path"
+import {Metadata} from "@grpc/grpc-js"
+import {grpcServiceClient} from "./client"
 
-const PROTO_PATH = path.resolve("./src/protos/server.proto")
-const packageDefinition = loadSync(
-  PROTO_PATH,
-  {
-    keepCase: true,
-    longs: String,
-    enums: String,
-    defaults: true,
-    oneofs: true
-  }
-)
 
-const apiServer = loadPackageDefinition(packageDefinition).protos
-
-const decodeFromBase64 = (str) => {
-  const decoded = Buffer.from(str, "base64").toString("ascii")
-  return Buffer.from(decoded)
-}
-
-const newGRPCClient = () => {
-  const caCert = decodeFromBase64(process.env.CA_CERT_BASE64)
-  const clientKey = decodeFromBase64(process.env.CLIENT_KEY_BASE64)
-  const clientCert = decodeFromBase64(process.env.CLIENT_CERT_BASE64)
-
-  const channelArgs = {}
-
-  if (process.env.GRPC_SERVER_SSL_TARGET_NAME_OVERRIDE){
-    channelArgs["grpc.ssl_target_name_override"] = process.env.GRPC_SERVER_SSL_TARGET_NAME_OVERRIDE
-  }
-
-  const client = new apiServer.CandidateTrackerGo(
-    process.env.GRPC_SERVER,
-    credentials.createSsl(caCert, clientKey, clientCert),
-    channelArgs
-  )
-  return client
-}
-
-const grpcClient = () => {
-  if (!global.gprcClient || global.refreshGrpcClient) {
-    global.refreshGrpcClient = false
-    console.log("Creating new grpc client")
-    global.gprcClient = newGRPCClient()
-  }
-
-  return global.gprcClient
-}
-
-const handleGrpcError = (reject, err) => {
+const handleGrpcError = (err) => {
   if(ErrorChecker.errorIsUnavailable(err)) {
     global.refreshGrpcClient = true
   }
-  reject(err)
+  console.log("err: ", err)
 }
 
 const metadataWithRequestingUserEmail = (userEmail) => {
@@ -66,18 +18,18 @@ const metadataWithRequestingUserEmail = (userEmail) => {
   return metadata
 }
 
-const checkConnection = (userEmail) => {
-  const client = grpcClient()
+const checkConnection = async (userEmail) => {
+  if (!userEmail) {
+    return
+  }
+  const md = metadataWithRequestingUserEmail(userEmail)
   const checkConnectionRequest = {}
-
-  return new Promise((resolve, reject) => {
-    client.CheckConnection(checkConnectionRequest, metadataWithRequestingUserEmail(userEmail), (err, checkConnectionResponse) => {
-      if (err) {
-        handleGrpcError(reject, err)
-      } else {
-        resolve(checkConnectionResponse)
-      }
-    })
+  return await grpcServiceClient().checkConnection(
+    checkConnectionRequest, md
+  ).then((checkConnectionResponse) => {
+    return checkConnectionResponse
+  }).catch((err) => {
+    handleGrpcError(err)
   })
 }
 
