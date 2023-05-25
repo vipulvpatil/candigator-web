254718c825a71de12c0c0f6d31ae961c8f1ec541
Vipul Vinod Patil <vipulvpatil@gmail.com>
2023-05-26 00:06:24 +0530

    added better icons

--- src/app/(dashboard)/menu.js
+++ src/app/(dashboard)/menu.js
@@ -1,23 +1,23 @@
 "use client"
 
-import DollarSignIcon from "@/icons/feather/dollar_sign"
+import BillingIcon from "@/icons/billing"
+import CandidatesIcon from "@/icons/candidates"
 import MenuButton from "./menu_button"
-import SearchIcon from "@/icons/feather/search"
-import SettingsIcon from "@/icons/feather/settings"
-import UserIcon from "@/icons/feather/user"
-import UsersIcon from "@/icons/feather/users"
+import ProfileIcon from "@/icons/profile"
+import SearchIcon from "@/icons/search"
+import SettingsIcon from "@/icons/settings"
 import {useSelectedLayoutSegment} from "next/navigation"
 
 const Menu = () => {
   const segment = useSelectedLayoutSegment()
   return (
     <div className="border-solid border-subtle border-r-2 w-[165px] flex flex-col">
-      <MenuButton title={"candidates"} selected={segment === "candidates"} icon={<UsersIcon/>}/>
+      <MenuButton title={"candidates"} selected={segment === "candidates"} icon={<CandidatesIcon/>}/>
       <MenuButton title={"search"} selected={segment === "search"} icon={<SearchIcon/>}/>
       <div className="flex-grow"/>
-      <MenuButton title={"billing"} selected={segment === "billing"} icon={<DollarSignIcon/>}/>
+      <MenuButton title={"billing"} selected={segment === "billing"} icon={<BillingIcon/>}/>
       <MenuButton title={"settings"} selected={segment === "settings"} icon={<SettingsIcon/>}/>
-      <MenuButton title={"profile"} selected={segment === "profile"} icon={<UserIcon/>}/>
+      <MenuButton title={"profile"} selected={segment === "profile"} icon={<ProfileIcon/>}/>
     </div>
   )
 }
--- src/app/(dashboard)/menu_button.js
+++ src/app/(dashboard)/menu_button.js
@@ -1,18 +1,18 @@
 import Link from "next/link"
 
 const MenuButton = ({title,  selected, icon}) => {
-  let displayClass = "text-black stroke-black hover:text-bold hover:bg-subtle/20 hover:stroke-bold cursor-pointer"
+  let displayClass = "text-black fill-black hover:text-bold hover:bg-subtle/20 hover:fill-bold cursor-pointer"
 
   if (selected) {
-    displayClass = "text-white bg-bold fill-white stroke-white"
+    displayClass = "text-white bg-bold fill-white"
   }
 
   return (
-    <Link href={`/${title}`} className={`block text-[24px] font-normal text-left ${displayClass}`}>
-      <div className="p-2 inline">
+    <Link href={`/${title}`} className={`block text-[24px] font-normal text-left py-1 ${displayClass}`}>
+      <div className="p-2 inline-flex align-middle w-[46px]">
         {icon}
       </div>
-      <div className="inline">
+      <div className="inline-flex align-middle fixed">
         {title}
       </div>
     </Link>
