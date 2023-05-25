49e3df252afc6ace25264cb779788a629162c2b6
Vipul Vinod Patil <vipulvpatil@gmail.com>
2023-05-25 16:31:44 +0530

    fixed unnecessary creation of excess Prisma clients during hotloading

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,9 +1,7 @@
 import GoogleProvider from "next-auth/providers/google"
 import NextAuth from "next-auth"
 import {PrismaAdapter} from "@next-auth/prisma-adapter"
-import {PrismaClient} from "@prisma/client"
-
-const prisma = new PrismaClient()
+import {prisma} from "../../../../../db/prisma"
 
 export const authOptions = {
   adapter: PrismaAdapter(prisma),
