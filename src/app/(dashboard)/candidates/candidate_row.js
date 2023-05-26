2516e8629546681ca3e2e437e8446450e8db3bbc
Vipul Vinod Patil <vipulvpatil@gmail.com>
2023-05-26 18:02:22 +0530

    styled the candidates page

--- src/app/(dashboard)/candidates/candidate_row.js
+++ src/app/(dashboard)/candidates/candidate_row.js
@@ -1,10 +1,42 @@
 const CandidateRow = ({candidate}) => {
-  return <>
-    <div>{candidate.name}</div>
-    <div>{candidate.designation}</div>
-    <div>{candidate.company}</div>
-    <div>{candidate.updated_at}</div>
-  </>
+  return <div className="contents cursor-pointer rowWrapper">
+    <div className="
+      col-span-2 pr-5 py-1
+      border-b-[1px] border-subtle
+      text-[28px] font-regular text-black/70
+    ">
+      <div className="line-clamp-1">
+        {candidate.name}
+      </div>
+    </div>
+    <div className="
+      col-span-2 pr-5 py-1
+      border-b-[1px] border-subtle
+      text-[24px] text-black/50
+    ">
+      <div className="line-clamp-1 relative top-[4px]">
+        {candidate.designation}
+      </div>
+    </div>
+    <div className="
+      col-span-2 pr-5 py-1
+      border-b-[1px] border-subtle
+      text-[24px] text-black/50
+    ">
+      <div className="line-clamp-1 relative top-[4px]">
+        {candidate.company}
+      </div>
+    </div>
+    <div className="
+      col-span-1 py-1
+      border-b-[1px] border-subtle
+      text-[24px] text-black/50 text-right
+    ">
+      <div className="line-clamp-1 relative top-[4px]">
+        {candidate.updated_at}
+      </div>
+    </div>
+  </div>
 }
 
 export default CandidateRow
--- src/app/(dashboard)/candidates/page.js
+++ src/app/(dashboard)/candidates/page.js
@@ -1,6 +1,6 @@
 import CandidateList from "./candidate_list"
+import CandidatesIcon from "@/icons/candidates"
 import GrpcService from "@/lib/grpc/service"
-import PageTitle from "@/app/(dashboard)/page_title"
 import StatusBox from "@/app/status_box"
 import {authOptions} from "@/app/api/auth/[...nextauth]/route"
 import {getServerSession} from "next-auth"
@@ -15,8 +15,13 @@ const Candidates = async () => {
 
   return (
     <>
-      <PageTitle title="Candidates"/>
-      <div className="grid grid-cols-4">
+      <div className="inline-flex align-middle w-[44px] fill-black/70 relative top-[-19px] left-[-4px]">
+        <CandidatesIcon/>
+      </div>
+      <div className="inline-flex text-[32px] font-regular text-black/70 relative top-[-9px] left-[-4px]">
+        {candidateList.length} candidates in total
+      </div>
+      <div className="grid grid-cols-7 border-t-[1px] border-subtle">
         <CandidateList candidates={candidateList}/>
       </div>
       <StatusBox/>
@@ -24,14 +29,23 @@ const Candidates = async () => {
   )
 }
 
+const dummy_candidate_list = [
+  {name: "Vipul Patil", designation: "Senior Software Engineer", company: "Ai Retreat Inc.", updated_at: "10 days ago"},
+  {name: "Vipul Vinod Patil", designation: "Senior Software Engineer Extraordinaire", company: "Confusing Labs Incorporated.", updated_at: "2 days ago"},
+  {name: "Patil Vipul", designation: "Really Senior Software Engineer", company: "Boincase Inc.", updated_at: "1 day ago"},
+  {name: "Mr. Vipul V Patil", designation: "Senior Staff Software Engineer", company: "Local company LLC", updated_at: "63 days ago"},
+  {name: "Vipul V. Patil", designation: "Junior Software Engineer", company: "Jobless", updated_at: "14 days ago"},
+]
+
 const getCandidateListFor = async (userEmail) => {
   // This code will return default data until the GRPC Service function is defined.
   const func = GrpcService.getCandidateList || (
     () => {
       const count = Math.floor(Math.random() * (15 - 5 + 1) + 5)
 
-      return [...Array(count)].map(() => {
-        return {name: "Vipul V Patil", designation: "Senior Software Engineer", company: "Ai Retreat Inc.", updated_at: "20 days ago"}
+      return [...Array(count)].map((value, i) => {
+        const candidate = dummy_candidate_list[i%dummy_candidate_list.length]
+        return candidate
       })
     }
   )
