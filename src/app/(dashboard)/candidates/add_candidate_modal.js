13407736c8bc0c3d43f9cab290314c3c662382d8
Vipul Vinod Patil <vipulvpatil@gmail.com>
2023-05-31 20:17:55 +0530

    added file selector button

--- src/app/(dashboard)/candidates/add_candidate_modal.js
+++ src/app/(dashboard)/candidates/add_candidate_modal.js
@@ -1,3 +1,5 @@
+import AddCandidateIcon from "@/icons/add_candidate"
+
 const AddCandidateModal = ({show, handleClose}) => {
   if (!show) {
     return <></>
@@ -20,9 +22,24 @@ const AddCandidateModal = ({show, handleClose}) => {
           min-h-[500px]
         bg-white
           drop-shadow-modal col-span-5
+          text-center
         "
       >
-        This is where you upload files
+        <div className="text-[32px]">Add candidates</div>
+        <div className="">Select one or more PDF files to upload</div>
+        <button className="
+          bg-bold hover:bg-dark text-white text-[18px]
+          fill-white rounded p-[10px] m-6
+          drop-shadow-button"
+          onClick={()=>{}}
+        >
+          <div className="inline-flex align-middle w-[28px] relative top-[-2px]">
+            <AddCandidateIcon/>
+          </div>
+          <div className="pl-2 pr-1 inline-flex align-middle relative top-[-2px]">
+            {"Choose File"}
+          </div>
+        </button>
       </div>
       <div className="col-span-1"></div>
     </>
