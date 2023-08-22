5a68631925d1e4aa5ece8b8b33d5250cddf5150e
Vipul Vinod Patil <vipulvpatil@gmail.com>
2023-08-22 15:24:50 +0530

    styled the entire candidate details page

--- src/app/(dashboard)/candidates/candidate_details.js
+++ src/app/(dashboard)/candidates/candidate_details.js
@@ -1,59 +1,25 @@
+import {
+  CandidateCertDetails,
+  CandidateContactDetails,
+  CandidateEducationDetails,
+  CandidateExperienceDetails,
+  CandidateRoleDetails,
+  CandidateSimpleElement,
+  CandidateSkillDetails,
+} from "./candidate_details_elements"
 import {useEffect, useState} from "react"
-import CandidateDetailElement from "./candidate_details_elements"
 import CloseIcon from "@/icons/close"
-
-const labelFont = "text-[18px] font-normal text-black/50"
-const valueFont = "text-[20px] font-semibold text-black/70"
-const subValueFont = "text-[18px] font-normal text-black/70"
 
 const CandidateDetails = ({candidate, onClose}) => {
   const [candidatePersona, setCandidatePersona] = useState(null)
-  const [candidateName, setCandidateName] = useState(null)
-  const [candidateContactDetails, setCandidateContactDetails] = useState(null)
-  const [candidateExperience, setCandidateExperience] = useState(null)
-  const [candidateEducation, setCandidateEducation] = useState(null)
   useEffect(() => {
     if (!candidate) {
       setCandidatePersona(null)
-      setCandidateName(null)
-      setCandidateContactDetails(null)
-      setCandidateExperience(null)
-      setCandidateEducation(null)
-      return
+    } else {
+      const displayPersona = candidate.displayPersona
+      console.log(displayPersona)
+      setCandidatePersona(displayPersona)
     }
-
-    const displayPersona = candidate.displayPersona
-    console.log(displayPersona)
-
-    setCandidateName(displayPersona["Name"])
-
-    setCandidateContactDetails({
-      "Email": displayPersona["Email"],
-      "Phone": displayPersona["Phone"],
-      "City": displayPersona["City"],
-      "State": displayPersona["State"],
-      "Country": displayPersona["Country"]
-    })
-
-    setCandidateExperience({
-      "YoE": displayPersona["YoE"],
-      "Experience": displayPersona["Experience"]
-    })
-
-    setCandidateEducation({
-      "Education": displayPersona["Education"]
-    })
-
-    setCandidatePersona({
-      "Tech Skills": displayPersona["Tech Skills"],
-      "Soft Skills": displayPersona["Soft Skills"],
-      "Recommended Roles": displayPersona["Recommended Roles"],
-      "Certifications": displayPersona["Certifications"],
-      "BuilderVersion": displayPersona["BuilderVersion"],
-      "BuiltBy": displayPersona["BuiltBy"],
-      "FileUploadId": displayPersona["FileUploadId"]
-    })
-
   }, [candidate])
 
   if (!candidatePersona) {
@@ -80,141 +46,34 @@ const CandidateDetails = ({candidate, onClose}) => {
         <CloseIcon/>
       </button>
       <div className="">
-        <div className="text-[24px] font-normal text-black/70 leading-none">
-          {candidate.id}
+        <div className="text-[18px] font-normal text-black/50 leading-snug">
+          {"id: "+candidate.id}
         </div>
+        <div className="text-[28px] font-semibold text-black/70 leading-snug">
+        {candidatePersona["Name"]}
       </div>
-      <CandidateSimpleElement label={"Name"} value={candidateName}/>
+      </div>
       <div className="h-3"/>
-      <CandidateContactDetails candidateContactDetails={candidateContactDetails}/>
+      <CandidateContactDetails candidatePersona={candidatePersona}/>
       <div className="h-4"/>
-      <CandidateExperienceDetails candidateExperience={candidateExperience}/>
+      <CandidateExperienceDetails candidatePersona={candidatePersona}/>
       <div className="h-4"/>
-      <CandidateEducationDetails candidateEducation={candidateEducation}/>
+      <CandidateEducationDetails candidatePersona={candidatePersona}/>
       <div className="h-4"/>
-      {Object.keys(candidatePersona).map((key) => {
-        const data = candidatePersona[key]
-        return <div key={key}>
-          <CandidateDetailElement label={key} value={data}/>
-        </div>
-      })}
+      <CandidateSkillDetails candidatePersona={candidatePersona}/>
+      <div className="h-4"/>
+      <CandidateRoleDetails candidatePersona={candidatePersona}/>
+      <div className="h-4"/>
+      <CandidateCertDetails candidatePersona={candidatePersona}/>
+      <div className="h-4"/>
+      <CandidateSimpleElement label={"BuilderVersion"} value={candidatePersona["BuilderVersion"]}/>
+      <CandidateSimpleElement label={"BuiltBy"} value={candidatePersona["BuiltBy"]}/>
+      <div className="h-1"/>
+      <CandidateSimpleElement label={"FileUploadId"} value={candidatePersona["FileUploadId"]}/>
+      <div className="h-1"/>
     </div>
   )
 }
 
-const CandidateContactElement = ({label, value}) => {
-  return <>
-    {label && <div className={`${labelFont}`}>
-      {label}
-    </div>}
-    {value && <div className={`${valueFont} leading-none overflow-x-scroll overflow-y-clip`}>
-      {value}
-    </div>}
-  </>
-}
-
-const CandidateContactDetails = ({candidateContactDetails}) => {
-  const email = candidateContactDetails["Email"]
-  const phone = candidateContactDetails["Phone"] || "N/A"
-  const city = candidateContactDetails["City"] || "N/A"
-  const state = candidateContactDetails["State"] || "N/A"
-  const country = candidateContactDetails["Country"] || "N/A"
-
-  return <div className="border-solid border-2 border-subtle/50 rounded-md clear-both">
-    <div className="clear-both">
-      <div className="w-[50%] inline-block border-subtle/50 border-r-2 border-b-2 p-1">
-        <CandidateContactElement label="Email" value={email}/>
-      </div>
-      <div className="w-[50%] inline-block border-subtle/50 border-b-2 p-1">
-        <CandidateContactElement label="Phone" value={phone}/>
-      </div>
-    </div>
-    <div className="clear-both">
-      <div className="w-[33%] inline-block border-subtle/50 border-r-2 p-1">
-        <CandidateContactElement label="City" value={city}/>
-      </div>
-      <div className="w-[33%] inline-block border-subtle/50 border-r-2 p-1">
-        <CandidateContactElement label="State" value={state}/>
-      </div>
-      <div className="w-[33%] inline-block p-1">
-        <CandidateContactElement label="Country" value={country}/>
-      </div>
-    </div>
-  </div>
-}
-
-const CandidateExperienceDetails = ({candidateExperience}) => {
-  const data = candidateExperience["Experience"] || []
-  return <div className="border-solid border-2 border-subtle/50 rounded-md p-1">
-    <CandidateSimpleElement label={"Years of experience"} value={candidateExperience["YoE"]}/>
-    {data.map((value, i) => {
-      let spaceElement = <div className="h-1"/>
-      let element = <CandidateExperienceElement values={value}/>
-      return <div key={i} className="border-solid border-t-2 border-subtle/50">
-        {spaceElement}
-        {element}
-      </div>
-    })}
-  </div>
-}
-
-const CandidateExperienceElement = ({values}) => {
-  return <div>
-    <CandidateSimpleElement value={values["Title"]}/>
-    <CandidateDetailLeftAndRightFloat
-      leftValue={values["Company Name"]}
-      rightValue={`${values["Starting Year"]} - ${values["Ending Year"]}`}
-    />
-  </div>
-}
-
-const CandidateEducationDetails = ({candidateEducation}) => {
-  const data = candidateEducation["Education"] || []
-  return <div className="border-solid border-2 border-subtle/50 rounded-md p-1">
-    <CandidateSimpleElement label={"Education"}/>
-    {data.map((value, i) => {
-      let spaceElement = <div className="h-1"/>
-      let element = <CandidateEducationElement values={value}/>
-      return <div key={i} className="border-solid border-t-2 border-subtle/50">
-        {spaceElement}
-        {element}
-      </div>
-    })}
-  </div>
-}
-
-const CandidateEducationElement = ({values}) => {
-  return <div>
-    <CandidateSimpleElement value={values["Institute"]}/>
-    <CandidateDetailLeftAndRightFloat
-      leftValue={values["Qualification"]}
-      rightValue={values["CompletionYear"]}
-    />
-  </div>
-}
-
-const CandidateDetailLeftAndRightFloat = ({leftValue, rightValue}) => {
-  return <>
-    <div className={`${subValueFont} py-1 leading-none float-left`}>
-      {leftValue}
-    </div>
-    <div className={`${subValueFont} py-1 leading-none float-right`}>
-      {rightValue}
-    </div>
-    <div className="clear-both"/>
-  </>
-}
-
-const CandidateSimpleElement = ({label, value}) => {
-  return <>
-    {label && <div className={`${labelFont} inline-block`}>
-      {label}
-    </div>}
-    {value && <div className={`${valueFont} leading-none`}>
-      {value}
-    </div>}
-  </>
-
-}
 
 export default CandidateDetails
--- src/app/(dashboard)/candidates/candidate_details_elements.js
+++ src/app/(dashboard)/candidates/candidate_details_elements.js
@@ -1,128 +1,21 @@
-let isString = value => typeof value === "string"
+const labelFont = "text-[18px] font-normal text-black/50"
+const valueFont = "text-[20px] font-semibold text-black/70"
+const subValueFont = "text-[18px] font-normal text-black/70"
 
-const allowedArray = [
-  "Tech Skills",
-  "Soft Skills",
-  "Recommended Roles",
-  "Education",
-  "Experience",
-  "BuilderVersion",
-  "BuiltBy",
-  "FileUploadId"
-]
-
-const allowedObjectKeys = {
-  "Education": [
-    "Institute",
-    "Qualification",
-    "CompletionYear",
-  ],
-  "Experience": [
-    "Title",
-    "Company Name",
-    "Starting Year",
-    "Ending Year",
-    "Ongoing",
-  ]
-}
-
-const CandidateDetailElement = ({label, value}) => {
-  let valueElement
-  if(Array.isArray(value)) {
-    if (allowedArray.includes(label)){
-      valueElement = <CandidateDetailArray label={label} values={value}/>
-    }
-  } else {
-    valueElement = <CandidateDetailText value={value}/>
-  }
+const CandidateContactElement = ({label, value}) => {
   return <>
-    <CandidateDetailLabel label={label}/>
-    {valueElement}
+    {label && <div className={`${labelFont}`}>
+      {label}
+    </div>}
+    {value && <div className={`${valueFont} leading-snug overflow-x-scroll overflow-y-clip`}>
+      {value}
+    </div>}
   </>
 }
 
-const CandidateDetailLabel = ({label}) => {
-  return <div className="pt-4 text-[18px] text-black/50 inline-block">
-    {label}
-  </div>
-}
-
-const CandidateDetailText = ({value}) => {
-  return <div className="text-[20px] font-semibold text-black/70 leading-none">
-    {value}
-  </div>
-}
-
-const CandidateDetailSubtext = ({value}) => {
-  return <div className="text-[18px] font-normal text-black/70 leading-none">
-    {value}
-  </div>
-}
-
-const CandidateDetailArray = ({label, values}) => {
-  return <>
-    {values.map((value, i) => {
-      return <div key={i}>
-        <CandidateDetailArrayElement label={label} value={value} index={i}/>
-      </div>
-    })}
-  </>
-}
-
-const CandidateDetailArrayElement = ({label, value, index}) => {
-  let element = null
-  let spaceElement = null
-  if(isString(value)) {
-    if (index !== 0) {
-      spaceElement = <div className="h-1"/>
-    }
-    element = <CandidateDetailText value={value}/>
-  } else {
-    if (index !== 0) {
-      spaceElement = <div className="h-2"/>
-    }
-    if(Object.keys(allowedObjectKeys).includes(label)){
-      element = <CandidateDetailObject label={label} values={value}/>
-    }
-  }
-  return <>
-    {spaceElement}
-    {element}
-  </>
-}
-
-const CandidateDetailObject = ({label, values}) => {
-  const keysToDisplay = allowedObjectKeys[label]
-  if(label === "Education") {
-    return <CandidateDetailEducation values={values}/>
-  }
-  if(label === "Experience") {
-    return <CandidateDetailExperience values={values}/>
-  }
-  return <>
-    {keysToDisplay.map((key, index) => {
-      const data = values[key]
-      if (data !== null){
-        if (index == 0){
-          return <CandidateDetailText key={key} value={data}/>
-        } else {
-          return <CandidateDetailSubtext key={key} value={data}/>
-        }
-      }
-    })}
-  </>
-}
-
-const CandidateDetailEducation = ({values}) => {
+const CandidateExperienceElement = ({values}) => {
   return <div>
-    <CandidateDetailText value={values["Institute"]}/>
-    <CandidateDetailLeftAndRightFloat leftValue={values["Qualification"]} rightValue={values["CompletionYear"]}/>
-  </div>
-}
-
-const CandidateDetailExperience = ({values}) => {
-  return <div>
-    <CandidateDetailText value={values["Title"]}/>
+    <CandidateSimpleElement value={values["Title"]}/>
     <CandidateDetailLeftAndRightFloat
       leftValue={values["Company Name"]}
       rightValue={`${values["Starting Year"]} - ${values["Ending Year"]}`}
@@ -130,16 +23,170 @@ const CandidateDetailExperience = ({values}) => {
   </div>
 }
 
+const CandidateEducationElement = ({values}) => {
+  return <div>
+    <CandidateSimpleElement value={values["Institute"]}/>
+    <CandidateDetailLeftAndRightFloat
+      leftValue={values["Qualification"]}
+      rightValue={values["CompletionYear"]}
+    />
+  </div>
+}
+
 const CandidateDetailLeftAndRightFloat = ({leftValue, rightValue}) => {
   return <>
-    <div className="pt-1 text-[18px] font-normal text-black/70 leading-none float-left">
+    <div className={`${subValueFont} py-1 leading-snug float-left`}>
       {leftValue}
     </div>
-    <div className="pt-1 text-[18px] font-normal text-black/70 leading-none float-right">
+    <div className={`${subValueFont} py-1 leading-snug float-right`}>
       {rightValue}
     </div>
     <div className="clear-both"/>
   </>
 }
 
-export default CandidateDetailElement
+const CandidateSkillElement = ({label, values}) => {
+  return <>
+    {label && <div className={`${labelFont}`}>
+      {label}
+    </div>}
+    {values && Array.isArray(values) &&
+      values.map((value, i) => {
+        return <div key={i} className={`${valueFont} leading-snug overflow-x-scroll   overflow-y-clip flex`}>
+          <div className="px-2">
+            &bull;
+          </div>
+          <div className="inline-block">
+            {value}
+          </div>
+        </div>
+      })
+    }
+  </>
+}
+
+const CandidateArrayElement = ({label, values}) => {
+  return <>
+    {label && <div className={`${labelFont}`}>
+      {label}
+    </div>}
+    {values && Array.isArray(values) &&
+      values.map((value, i) => {
+        return <div key={i} className={`${valueFont} leading-snug overflow-x-scroll   overflow-y-clip flex`}>
+          <div className="px-2">
+            &bull;
+          </div>
+          <div className="inline-block">
+            {value}
+          </div>
+        </div>
+      })
+    }
+  </>
+}
+
+export const CandidateSimpleElement = ({label, value}) => {
+  return <>
+    {label && <div className={`${labelFont} inline-block`}>
+      {label}
+    </div>}
+    {value && <div className={`${valueFont} leading-snug`}>
+      {value}
+    </div>}
+  </>
+
+}
+
+export const CandidateContactDetails = ({candidatePersona}) => {
+  const email = candidatePersona["Email"]
+  const phone = candidatePersona["Phone"] || "N/A"
+  const city = candidatePersona["City"] || "N/A"
+  const state = candidatePersona["State"] || "N/A"
+  const country = candidatePersona["Country"] || "N/A"
+
+  return <div className="border-solid border-2 border-subtle/50 rounded-md clear-both">
+    <div className="clear-both">
+      <div className="w-[50%] inline-block border-subtle/50 border-r-2 border-b-2 p-1">
+        <CandidateContactElement label="Email" value={email}/>
+      </div>
+      <div className="w-[50%] inline-block border-subtle/50 border-b-2 p-1">
+        <CandidateContactElement label="Phone" value={phone}/>
+      </div>
+    </div>
+    <div className="clear-both">
+      <div className="w-[33%] inline-block border-subtle/50 border-r-2 p-1">
+        <CandidateContactElement label="City" value={city}/>
+      </div>
+      <div className="w-[33%] inline-block border-subtle/50 border-r-2 p-1">
+        <CandidateContactElement label="State" value={state}/>
+      </div>
+      <div className="w-[33%] inline-block p-1">
+        <CandidateContactElement label="Country" value={country}/>
+      </div>
+    </div>
+  </div>
+}
+
+export const CandidateExperienceDetails = ({candidatePersona}) => {
+  const data = candidatePersona["Experience"] || []
+  return <div className="border-solid border-2 border-subtle/50 rounded-md p-1">
+    <CandidateSimpleElement label={"Years of experience"} value={candidatePersona["YoE"]}/>
+    {data.map((value, i) => {
+      let spaceElement = <div className="h-1"/>
+      let element = <CandidateExperienceElement values={value}/>
+      return <div key={i} className="border-solid border-t-2 border-subtle/50">
+        {spaceElement}
+        {element}
+      </div>
+    })}
+  </div>
+}
+
+export const CandidateEducationDetails = ({candidatePersona}) => {
+  const data = candidatePersona["Education"] || []
+  return <div className="border-solid border-2 border-subtle/50 rounded-md p-1">
+    <CandidateSimpleElement label={"Education"}/>
+    {data.map((value, i) => {
+      let spaceElement = <div className="h-1"/>
+      let element = <CandidateEducationElement values={value}/>
+      return <div key={i} className="border-solid border-t-2 border-subtle/50">
+        {spaceElement}
+        {element}
+      </div>
+    })}
+  </div>
+}
+
+export const CandidateSkillDetails = ({candidatePersona}) => {
+  const techSkills = candidatePersona["Tech Skills"] || ["N/A"]
+  const softSkills = candidatePersona["Soft Skills"] || ["N/A"]
+
+  return <div className="border-solid border-2 border-subtle/50 rounded-md clear-both table table-fixed">
+    <div className="w-[50%] h-full table-cell border-subtle/50 border-r-2 p-1 align-top">
+      <CandidateSkillElement label="Technical skills" values={techSkills}/>
+    </div>
+    <div className="w-[50%] h-full table-cell border-subtle/50 p-1 align-top">
+      <CandidateSkillElement label="Soft skills" values={softSkills}/>
+    </div>
+  </div>
+}
+
+export const CandidateRoleDetails = ({candidatePersona}) => {
+  const roles = candidatePersona["Recommended Roles"] || ["N/A"]
+
+  return <div className="border-solid border-2 border-subtle/50 rounded-md clear-both">
+    <div className="w-[50%] h-full table-cell p-1 align-top">
+      <CandidateArrayElement label="Recommended Roles" values={roles}/>
+    </div>
+  </div>
+}
+
+export const CandidateCertDetails = ({candidatePersona}) => {
+  const certs = candidatePersona["Certificates"] || ["None"]
+
+  return <div className="border-solid border-2 border-subtle/50 rounded-md clear-both">
+    <div className="w-[50%] h-full table-cell p-1 align-top">
+      <CandidateArrayElement label="Certificates" values={certs}/>
+    </div>
+  </div>
+}
