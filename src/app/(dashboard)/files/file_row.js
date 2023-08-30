a51ce071c4a549f720e4edc2daa0acf958e9766d
Vipul Vinod Patil <vipulvpatil@gmail.com>
2023-08-30 19:08:22 +0530

    changed location of edit button on file list screen

--- src/app/(dashboard)/files/file_row.js
+++ src/app/(dashboard)/files/file_row.js
@@ -29,33 +29,30 @@ const FileRow = ({fileUpload, selected, setSelectedFileUploadId, showTopBorder})
       }}
     >
       <div className={`
-        col-span-4 pr-5 py-1 pl-1
+        col-span-5 pr-5 py-1 pl-1
         border-b-[1px] border-subtleColor/30
         text-[28px] text-black/70
         ${topBorder}
       `}>
-        <div className={`line-clamp-1 ${textFontStyle}`}>
+        <div className={`line-clamp-1 ${textFontStyle} inline`}>
           {fileUpload.name}
         </div>
-      </div>
-      <div className={`
-        col-span-3
-        py-1 pr-1
-        border-b-[1px] border-subtleColor/30
-        text-[24px] text-black/50 text-right
-        ${topBorder}
-      `}>
-        {selected && <div className="px-2 inline">
+        {selected && <div className="px-2 inline align-top relative top-[-4px]">
           <Link href={`/files/${fileUpload.id}`}>
             <button
-              className="
-                w-[34px]
-                fill-secondaryColor hover:fill-secondaryDarkColor"
+              className="w-[34px] fill-secondaryColor hover:fill-secondaryDarkColor"
             >
               <EditIcon/>
             </button>
           </Link>
         </div>}
+      </div>
+      <div className={`
+        col-span-2 py-1 pr-1
+        border-b-[1px] border-subtleColor/30
+        text-[24px] text-black/50 text-right
+        ${topBorder}
+      `}>
         <div className={`inline line-clamp-1 relative top-[4px] ${statusTextColor} ${textFontStyle}`}>
           {fileUpload.processingStatus}
         </div>
