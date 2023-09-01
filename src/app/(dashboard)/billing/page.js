092cc72cec88bb5e6c0c43caa4da784cf3e98d23
Vipul Vinod Patil <vipulvpatil@gmail.com>
2023-09-01 12:25:59 +0530

    added a billing page

--- src/app/(dashboard)/billing/page.js
+++ src/app/(dashboard)/billing/page.js
@@ -1,11 +1,21 @@
 import PageHeader from "@/components/page_header"
-import StatusBox from "@/app/status_box"
 
 const Billing = () => {
   return (
     <div className="min-h-[620px] bg-gray-200">
-      <PageHeader title={"Billing"}/>
-      <StatusBox/>
+      <PageHeader title={"Billing"}>
+
+      </PageHeader>
+      <div className="m-[22px]">
+        <div className="
+          font-semibold text-black/70
+          p-[22px] bg-white rounded-lg
+        ">
+          <div>{"Thanks for your interest in a paid version of this application."}</div>
+          <div>{"We are currently in Beta phase and do not have a paid version."}</div>
+          <div>{"If you are interested in a paid version, please reach out to us at vipulvpatil@gmail.com"}</div>
+        </div>
+      </div>
     </div>
   )
 }
