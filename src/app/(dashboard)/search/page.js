4da055be73d3547bb7176a4b84a7393d5f57b243
Vipul Vinod Patil <vipulvpatil@gmail.com>
2023-09-02 11:33:16 +0530

    added search page

--- src/app/(dashboard)/search/page.js
+++ src/app/(dashboard)/search/page.js
@@ -4,6 +4,14 @@ const Search = () => {
   return (
     <div className="min-h-[620px] bg-gray-200">
       <PageHeader title={"Search"}/>
+      <div className="m-[22px]">
+        <div className="
+          font-semibold text-black/70
+          p-[22px] bg-white rounded-lg
+        ">
+          <div>{"Select a search criteria to see specific candidates."}</div>
+        </div>
+      </div>
     </div>
   )
 }
