7b617d30a802d3d1e785de0a1ab9ed33e81d6409
Vipul Vinod Patil <vipulvpatil@gmail.com>
2023-09-07 21:58:40 +0530

    new landing page styling 4

--- src/components/landing/product_description.js
+++ src/components/landing/product_description.js
@@ -4,19 +4,20 @@ const ProductDescription = () => {
   return (
     <div className="
       w-[43%] min-w-[500px] max-w-[600px] h-[427px]
-      bg-white rounded-lg leading-2
+      bg-white rounded-lg leading-snug
+      flex flex-col justify-evenly
     ">
       <div className="
         flex justify-center font-semibold text-[24px] text-center
-        min-w-[380px] max-w-[380px] mx-auto mt-[30px]
+        min-w-[380px] max-w-[380px] mx-auto
       ">
         {"No more looking through resumes. Quickly find the right candidate for the right job."}
       </div>
       <div className="
-        font font-semibold text-[18px] mx-auto my-8
+        font font-semibold text-[18px] mx-auto
         min-w-[420px] max-w-[420px]
       ">
-        <div className="flex flex-col h-[145px] place-content-evenly">
+        <div className="flex flex-col h-[145px] justify-between">
           <div className="flex items-center">
             <div className="
               bg-secondaryColor w-5 h-5 rounded-xl mr-4
