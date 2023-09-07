1ec4fe764b7ed0025257a6ea2044317821dd1fc7
Vipul Vinod Patil <vipulvpatil@gmail.com>
2023-09-07 18:24:44 +0530

    new landing page styling 2

--- src/app/page.js
+++ src/app/page.js
@@ -34,12 +34,39 @@ const Home = () => {
         h-[1890px]
       ">
         <div className="w-full flex place-content-evenly py-12">
-          <div className="w-[43%] bg-white rounded-lg">
+          <div className="w-[43%] bg-white rounded-lg leading-2">
             <div className="
               flex justify-center font-semibold text-[24px] text-center
               min-w-[380px] max-w-[380px] mx-auto mt-[30px]
             ">
               {"No more looking through resumes. Quickly find the right candidate for the right job."}
+            </div>
+            <div className="
+              font font-semibold text-[18px] mx-auto mt-8
+              min-w-[420px] max-w-[420px]
+            ">
+              <div className="flex flex-col h-[145px] place-content-evenly">
+                <div className="flex items-center">
+                  <div className="
+                    bg-secondaryColor w-5 h-5 rounded-xl mr-4
+                  "></div>{"Process 100s of Resumes using Open AI."}
+                </div>
+                <div className="flex items-center">
+                  <div className="
+                    bg-secondaryColor w-5 h-5 rounded-xl mr-4
+                  "></div>{"Easily edit processed candidate information."}
+                </div>
+                <div className="flex items-center">
+                  <div className="
+                    bg-secondaryColor w-5 h-5 rounded-xl mr-4
+                  "></div>{"Advanced search on candidate date."}
+                </div>
+                <div className="flex items-center">
+                  <div className="
+                    bg-secondaryColor w-5 h-5 rounded-xl mr-4
+                  "></div>{"Bulk convert pdfs to searchable data."}
+                </div>
+              </div>
             </div>
           </div>
           <div className="
