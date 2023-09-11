bc9cc33dfafd1b3e83381b4fe933a02233afbb01
Vipul Vinod Patil <vipulvpatil@gmail.com>
2023-09-11 11:15:18 +0530

    added Google tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css"
 import {NextAuthProvider} from "./AuthProvider"
 import {Quicksand} from "next/font/google"
+import Script from "next/script"
 
 const quicksand = Quicksand({
   weight: ["400", "600", "700"],
@@ -17,6 +18,16 @@ export const metadata = {
 const RootLayout = ({children}) => {
   return (
     <html lang="en">
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-43N2P9YPN0" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-43N2P9YPN0');
+        `}
+      </Script>
       <body className={quicksand.variable}>
         <NextAuthProvider>
           {children}
